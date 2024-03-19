4754d2f88a4d8631b0b5eab7c109d52f4d044ac7
danngjvit <giadan@jv-it.com.vn>
2024-03-19 23:01:53 +0700

    Refactor file path

--- box_debug.php
+++ box_debug.php
@@ -10,41 +10,38 @@ License: GPLv2 or later
 Text Domain: boxtheme
 */
 
+defined( 'ABSPATH' ) || exit;
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
-define( 'BOX_VERSION', '3.18.3' );
+define( 'BOX_VERSION', '1.0' );
 
 define( 'BOX__FILE__', __FILE__ );
 define( 'BOX_PLUGIN_BASE', plugin_basename( BOX__FILE__ ) );
 define( 'BOX_PATH', plugin_dir_path( BOX__FILE__ ) );
-define( 'BOX_URL', plugins_url( '/', ELEMENTOR__FILE__ ) );
-
-
-define( 'BOX_MODULES_PATH', plugin_dir_path( ELEMENTOR__FILE__ ) . '/modules' );
+define( 'BOX_URL', plugins_url( '/', BOX__FILE__ ) );
+define( 'BOX_MODULES_PATH', plugin_dir_path( BOX__FILE__ ) . '/modules' );
 define( 'BOX_ASSETS_PATH', BOX_PATH . 'assets/' );
 define( 'BOX_ASSETS_URL', BOX_URL . 'assets/' );
+
 
 require BOX_PATH . 'includes/Box_Log.php';
 require BOX_PATH . 'includes/Box_Admin.php';
 
 
-
 Class Box_Debug{
     protected $act;
     function __construct(){
-        $this->act = isset($_GET['act']) ? $_GET['act'] : '';    
+        $this->act = isset($_GET['act']) || $_GET['act'] ;    
     }
 
 }
 new Box_Debug();
 
+
 function box_debug_activate() {
-
-    global $myvar;
-
     flush_rewrite_rules();
  }
  register_activation_hook( __FILE__, 'box_debug_activate' );
