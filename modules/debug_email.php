5aef74975674e2a37906cf35124c88013d6fead1
danng <danhoat@gmail.com>
2025-05-19 11:49:29 +0700

    add file

--- modules/debug_email.php
+++ modules/debug_email.php
@@ -4,6 +4,6 @@ add_action( 'wp_mail_failed', 'onMailError', 10, 1 );
 function onMailError( $wp_error ) {
     echo "<pre>";
     print_r($wp_error);
-    box_log($wp_error)
+    box_log($wp_error);
     echo "</pre>";
 }       
