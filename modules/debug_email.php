d4ad7c04eac93ab56a3841e301b3b96387c17463
danng <danhoat@gmail.com>
2025-05-19 11:47:56 +0700

    add file

---
+++ modules/debug_email.php
@@ -0,0 +1,9 @@
+<?php 
+
+add_action( 'wp_mail_failed', 'onMailError', 10, 1 );
+function onMailError( $wp_error ) {
+    echo "<pre>";
+    print_r($wp_error);
+    box_log($wp_error)
+    echo "</pre>";
+}       
--- woo/add_extra_fee.php
+++ woo/add_extra_fee.php
@@ -1,26 +1,10 @@
 <?php 
 
-
-
-// require BOX_PATH .'/woo/add_extra_fee.php';
-
-function wk_add_text_field() { ?>
-    <div class="custom-field-wrap" style="margin: 10px;">
-        <label for="custom-field"><?php esc_html_e( 'Quote', 'webkul' ); ?></label>
-        <input type="text" name='custom-field' id='custom-field' value=''>
-    </div>
-    <?php
-}
-add_action( 'woocommerce_before_add_to_cart_button', 'wk_add_text_field' );
-
 /**
  *No.3 
  **/
 function wk_add_to_cart_validation( $passed, $product_id, $quantity, $variation_id = null ) {
-    // echo '<pre>';
-    // var_dump($_POST);
-    // echo '</pre>';
-    // die();
+   
     if ( empty( $_POST['custom-field'] ) ) {
         $passed = false;
         wc_add_notice( __( 'Quote is a required field.', 'webkul' ), 'error' );
@@ -35,7 +19,7 @@ add_filter( 'woocommerce_add_to_cart_validation', 'wk_add_to_cart_validation', 1
  * No.4
  * Add custom cart item data
  */
-function wk_add_cart_item_data( $cart_item_data, $product_id, $variation_id ) {
+function box_add_cart_item_data( $cart_item_data, $product_id, $variation_id ) {
     if ( isset( $_POST['custom-field'] ) ) {
         $cart_item_data['pr_field'] = sanitize_text_field( $_POST['custom-field'] );
     }
@@ -44,14 +28,14 @@ function wk_add_cart_item_data( $cart_item_data, $product_id, $variation_id ) {
     }
     return $cart_item_data;
 }
-add_filter( 'woocommerce_add_cart_item_data', 'wk_add_cart_item_data', 10, 3 );
+add_filter( 'woocommerce_add_cart_item_data', 'box_add_cart_item_data', 10, 3 );
 
 
 
 /**
  * Display custom item data in the cart
  */
-function wk_get_item_data( $item_data, $cart_item_data ) {
+function box_get_item_data( $item_data, $cart_item_data ) {
     if ( isset( $cart_item_data['pr_field'] ) ) {
         $item_data[] = array(
             'key'   => __( 'Quote', 'webkul' ),
@@ -69,7 +53,7 @@ function wk_get_item_data( $item_data, $cart_item_data ) {
     
     return $item_data;
 }
-add_filter( 'woocommerce_get_item_data', 'wk_get_item_data', 10, 2 );
+add_filter( 'woocommerce_get_item_data', 'box_get_item_data', 10, 2 );
 
 
 
@@ -77,7 +61,7 @@ add_filter( 'woocommerce_get_item_data', 'wk_get_item_data', 10, 2 );
 /**
  * Add custom meta to order
  */
-function wk_checkout_create_order_line_item( $item, $cart_item_key, $values, $order ) {
+function box_checkout_create_order_line_item( $item, $cart_item_key, $values, $order ) {
     if ( isset( $values['pr_field'] ) ) {
         $item->add_meta_data(
             __( 'Quote', 'webkul' ),
@@ -92,5 +76,12 @@ function wk_checkout_create_order_line_item( $item, $cart_item_key, $values, $or
             true
         );
     }
+    if ( isset( $values['bundles'] ) ) {
+        $item->add_meta_data(
+            __( 'Delivery date', 'webkul' ),
+            $values['delivery_date'],
+            true
+        );
+    }
 }
-add_action( 'woocommerce_checkout_create_order_line_item', 'wk_checkout_create_order_line_item', 10, 4 );
+add_action( 'woocommerce_checkout_create_order_line_item', 'box_checkout_create_order_line_item', 10, 4 );
